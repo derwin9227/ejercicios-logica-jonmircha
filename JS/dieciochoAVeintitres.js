7d7fe8b370485e03bac9e2e2e94a9c4a3184fa3f
derwin <derwin9227@gmail.com>
2023-02-27 17:03:48 -0500

    ejercicio 20 resuelto

--- JS/dieciochoAVeintitres.js
+++ JS/dieciochoAVeintitres.js
@@ -23,15 +23,34 @@ dividirLetras("Hola Mundo");
 console.log(`la frase → ${frase.texto} ← tiene ${frase.vocales.length} vocales y ${frase.consonantes.length} consonantes`); */
 
 
-//19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+//19) Programa una función que valide que un texto
+// sea un nombre válido, pe. miFuncion("Jonathan MirCha")
+// devolverá verdadero.
 const validarNombre = nombre => (typeof nombre === "string" && nombre !== "" && (/^[a-z\s]+$/ig.test(nombre)));
+
 const evaluarNombre = nombre => validarNombre(nombre) ? console.log(`→ ${nombre} ← es un nombre valido`) : console.log(`→ ${nombre} ← no es un nombre valido`);
-evaluarNombre("hola 5 veces");
+
+/* evaluarNombre("hola 5 veces");
 evaluarNombre("derwin hernandez");
 evaluarNombre("derwin");
-evaluarNombre("hola mundo.");
+evaluarNombre("hola mundo."); */
 
 //20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+const validarEmail = email => ((/[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(email)));
+console.log(validarEmail(""));
+console.log(validarEmail(546));
+console.log(validarEmail({}));
+console.log(validarEmail([]));
+console.log(validarEmail(true));
+console.log(validarEmail(undefined));
+console.log(validarEmail());
+console.log(validarEmail("derw_-.5_64nh@gl.cv"));
+
 //21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+
+
+
 //22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+
+
 //23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
